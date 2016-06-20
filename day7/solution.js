afb731168acd163ee78e823b1985407e533b258c
Brian Kemper <bkemper@gmail.com>
2016-06-20 09:35:30 -0600

    day7 part2 done

--- day7/solution.js
+++ day7/solution.js
@@ -15,7 +15,6 @@ function makeInstr(str) {
             operator = 'NOT';
             first = e.split(` -> `)[0].split(` `)[1];
             first = /[0-9]/.test(first) ? ~~first : first;
-
             second = null;
         } else if (e.split(` -> `)[0].split(` `).length === 3) {
             operator = e.split(` -> `)[0].split(` `)[1];
@@ -78,4 +77,56 @@ function emulate(str) {
     console.log(obj);
 }
 
-emulate(inp);
+// emulate(inp);
+
+//part2
+
+function emulatep2(str) {
+    let instr = makeInstr(str),
+        obj = {};
+    while (instr.length) {
+        for (let i = instr.length - 1; i >= 0; i--) {
+            let {
+                first,
+                operator,
+                second,
+                wire
+            } = instr[i];
+            if (wire === 'b') {
+                first = 46065;
+            }
+            if (operator === 'STRAIGHT') {
+                if (/[0-9]/.test(first)) {
+                    obj[wire] = first;
+                    instr.splice(i, 1);
+                } else if (obj.hasOwnProperty(first)) {
+                    obj[wire] = obj[first];
+                    instr.splice(i, 1);
+                }
+            } else if (operator === 'NOT' && obj.hasOwnProperty(first)) {
+                obj[wire] = obj[first] ^ 65535;
+                instr.splice(i, 1);
+            } else if (operator === 'RSHIFT' && obj.hasOwnProperty(first)) {
+                obj[wire] = obj[first] >> second;
+                instr.splice(i, 1);
+            } else if (operator === 'LSHIFT' && obj.hasOwnProperty(first)) {
+                obj[wire] = obj[first] << second;
+                instr.splice(i, 1);
+            } else if (operator === 'AND' && first && second) {
+                if (/[0-9]/.test(first) && obj.hasOwnProperty(second)) {
+                    obj[wire] = first & obj[second];
+                    instr.splice(i, 1);
+                } else if (obj.hasOwnProperty(first) && obj.hasOwnProperty(second)) {
+                    obj[wire] = obj[first] & obj[second];
+                    instr.splice(i, 1);
+                }
+            } else if (operator === 'OR' && obj.hasOwnProperty(first) && obj.hasOwnProperty(second)) {
+                obj[wire] = obj[first] | obj[second];
+                instr.splice(i, 1);
+            }
+        }
+    }
+    console.log(obj);
+}
+
+emulatep2(inp);
